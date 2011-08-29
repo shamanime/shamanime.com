4dbbe7249369315960026cef0b7b5c0225d611fb
Jefferson Queiroz Venerando <jefferson@shamanime.com>
2011-08-29 02:25:09 -0300

    Will paginate v3 breaks pagination, using v2 instead. Hacking its link renderer to work with layout, needs refactoring

--- Gemfile
+++ Gemfile
@@ -8,20 +8,20 @@ gem 'rails', '3.1.0.rc6'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails', " ~> 3.1.0.rc"
+  gem 'sass-rails', "~> 3.1.0.rc"
   gem 'coffee-rails', "~> 3.1.0.rc"
   gem 'uglifier'
 end
 
 gem 'jquery-rails'
 gem 'bson_ext', ">= 1.3.1"
-gem 'mongoid', ">= 2.1.9"
+gem 'mongoid', '>= 2.2.0'
 gem 'devise', ">= 1.4.2"
 gem 'haml-rails'
 gem 'high_voltage'
 gem 'mongoid_slug'
 gem 'rdiscount'
-gem 'will_paginate'
+gem 'will_paginate', '2.3.16'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- config/initializers/custom_will_paginate.rb
+++ config/initializers/custom_will_paginate.rb
@@ -1,33 +1,58 @@
-class PaginationListLinkRenderer < WillPaginate::ViewHelpers::LinkRenderer
+class PaginationListLinkRenderer < WillPaginate::LinkRenderer
+  def to_html
+    links = @options[:page_links] ? windowed_links : []
+    # previous/next buttons
+    links.unshift page_link_or_span(@collection.previous_page, 'disabled prev', @options[:previous_label])
+    links.push page_link_or_span(@collection.next_page, 'disabled next', @options[:next_label])
+  
+    html = links.join(@options[:separator])
+    html = html.html_safe if html.respond_to? :html_safe
+    @options[:container] ? @template.content_tag(:ul, html, html_attributes) : html
+  end
+  
+  def gap_marker
+    @gap_marker ||= begin
+      gap_text = @template.will_paginate_translate(:page_gap) { '&hellip;' }
+      %(<li class="gap">#{gap_text}</li>)
+    end
+  end
+  
   protected
-    def page_number(page)
-      unless page == current_page
-        tag(:li, link(page, page, :rel => rel_value(page)))
-      else
-        tag(:li, link(page, page, :rel => rel_value(page)), :class => "active")
-      end
-    end
-    
-    def previous_page
-      num = @collection.current_page > 1 && @collection.current_page - 1
-      previous_or_next_page(num, @options[:previous_label], 'prev')
-    end
+  # Collects link items for visible page numbers.
+  def windowed_links
+    prev = nil
 
-    def next_page
-      num = @collection.current_page < @collection.total_pages && @collection.current_page + 1
-      previous_or_next_page(num, @options[:next_label], 'next')
+    visible_page_numbers.inject [] do |links, n|
+      # detect gaps:
+      links << gap_marker if prev and n > prev + 1
+      links << page_link_or_span(n, 'active')
+      prev = n
+      links
     end
+  end
+  
+  def page_link_or_span(page, span_class, text = nil)
+    text ||= page.to_s
+    text = text.html_safe if text.respond_to? :html_safe
 
-    def previous_or_next_page(page, text, classname)
-      if page
-        tag(:li, link(text, page), :class => classname)
-      else
-        tag(:li, link(text, page), :class => classname + ' disabled')
-      end
+    if page and page != current_page
+      classnames = span_class && span_class.index(' ') && span_class.split(' ', 2).last
+      page_link page, text, :rel => rel_value(page), :class => classnames
+    else
+      page_disabled page, text, :class => span_class
     end
+  end
 
-    def html_container(html)
-      tag(:ul, html, container_attributes)
+  def page_link(page, text, attributes = {})
+    @template.content_tag :li, attributes do
+      @template.link_to text, url_for(page), attributes
     end
+  end
+  
+  def page_disabled(page, text, attributes = {})
+    @template.content_tag :li, attributes do
+      @template.link_to text,'#', attributes
+    end
+  end
 end
 WillPaginate::ViewHelpers.pagination_options[:renderer] = 'PaginationListLinkRenderer'
