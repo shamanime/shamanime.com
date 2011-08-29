b5993042c336a1a597b7e55129bb9f3272565a82
Jefferson Queiroz Venerando <jefferson@shamanime.com>
2011-08-29 15:09:16 -0300

    Editing default seed info

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,5 @@
 puts 'EMPTY THE MONGODB DATABASE'
 Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'jefferson', :username => 'jefferson', :email => 'jefferson@shamanime.com', :password => '123456', :password_confirmation => '123456'
+user = User.create! :name => 'Administrator', :username => 'admin', :email => 'admin@localhost.com', :password => '123456', :password_confirmation => '123456'
 puts 'New user created: ' << user.name
-post = Post.create! :title => 'First Post', :body => 'Nunc varius ultrices venenatis. Pellentesque vulputate blandit dolor vel pellentesque. Mauris porttitor elit sed diam feugiat ut adipiscing ante auctor. Cras est enim, aliquam id tempus sit amet, accumsan in sem. Duis mi mauris, porta ut adipiscing id, elementum nec urna. Maecenas nec velit ante. In facilisis tempor aliquet. Duis enim ligula, lobortis a laoreet non, lobortis id eros.', :published_on => Time.now, :user => user
-puts 'New post created: ' << post.title
